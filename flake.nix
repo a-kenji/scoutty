b6051bcbd68b427d64367b1a9940ecddd67f3435
a-kenji <aks.kenji@protonmail.com>
2026-03-21 14:57:42 +0100

    nix/flake: Remove description

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
-  description = "scoutty - Terminal capability probe CLI";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-
   outputs = args: import ./nix args;
 }
